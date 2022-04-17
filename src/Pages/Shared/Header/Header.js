69b29fe0f0b124fde9f0a57a2839a44f1e00c24b
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-17 16:02:17 -0700

    sign out button add

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,8 +3,17 @@ import { Link } from "react-router-dom";
 import CustomLink from '../../CustomLink/CustomLink';
 import logo from '../../../image/logo.png'
 import './Header.css'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
+
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <>
             <Navbar collapseOnSelect expand="lg" sticky="top" bg="primary" variant="dark">
@@ -27,9 +36,13 @@ const Header = () => {
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
-                            <Nav.Link as={Link} to="login">
-                                Login
-                            </Nav.Link>
+                            {
+                                user ?
+                                    <button onClick={handleSignOut}>Sign out</button>
+                                    :
+                                    <Nav.Link as={Link} to="login">
+                                        Login
+                                    </Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
