5c170bd1183ff4e698b735d0efbe3205309e24ad
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-17 18:18:08 -0700

    terms and condition add and register button disabled

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -42,9 +42,13 @@ const Register = () => {
                 <input type="email" name="email" id="" placeholder='Your Email' required />
 
                 <input type="password" name="password" id="" placeholder='Your Password' required />
-                <input className='mx-2' onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
-                <label className={agree ? 'text-primary' : 'text-danger'} htmlFor='terms'>Accept Learn with Ayman Sadik terms and conditions</label>
-                <input className='w-50 mx-auto btn btn-primary mt-2' type="submit" value="Register" />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor='terms'>Accept Learn with Ayman Sadik terms and conditions</label>
+                <input
+                    disabled={!agree}
+                    className='w-50 mx-auto btn btn-primary mt-2'
+                    type="submit"
+                    value="Register" />
             </form>
             <p className='mt-4'>Already have an Account?
                 <Link to='/login' className='text-primary mx-2 pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
