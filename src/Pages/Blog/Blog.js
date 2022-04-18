c550268b74ba78ad0b75e96eb1ef2b7047cf586a
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-18 02:02:29 -0700

    answer the question in blog route

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -19,7 +19,11 @@ const Blog = () => {
             </div>
             <div>
                 <h1>Question 2: Why are you using firebase? What other options do you have to implement authentication?</h1>
-                <h4><span>Answer:</span>{``
+                <h4><span>Answer:</span>{`Firebase offers many features as the backend development tool for web and mobile apps.It reduce workload and time.It is a prototyping tool.It is simple easy to use and industrially recognize.
+                The alternative of google firebase for authentication is- Auth0,MongoDB,passport,okta,keycloak.`
+                }</h4>
+                <h1>Question 3: What other services does firebase provide other than authentication</h1>
+                <h4><span>Answer:</span>{`Expect authentication firebase can provide many services.Such as - firebase authentication provide backend services,easy to use SDKs,ready made UI libraries to authenticate users to your app.It also provides cloud storage,cloud function,hosting cloud storage,cloud messaging and google analytics.`
                 }</h4>
             </div>
         </div>
