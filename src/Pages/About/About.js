7f26610b8039a0f073f55529e7106f4bd49ce9e6
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-18 02:39:33 -0700

    about me route

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,9 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>About</h2>
+            <h1 className='text-center mt-4'>About Me</h1>
+            <h3>Name: Tashfia Islam</h3>
+            <h4>I live in Vancouver BC Canada.Currently I am working in a farm as a grower.I completed my graduation in Computer Science and Engineering from Bangladesh.I was not good in programming but I always want to be a good programmer.My next goal to get a job in canada as a web developer.I want to be a creative successful web developer.After doing job I am going with the course flow.I want to finish this course and want to join SCIC. </h4>
         </div>
     );
 };
